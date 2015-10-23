59e0861fc327df9df85a38592d202e12c4f116ca
Nick Fragkoulis <frag18nik@gmail.com>
2015-10-23 21:30:35 +0300

    Update sylvester.h

--- sylvester.h
+++ sylvester.h
@@ -1,7 +1,7 @@
 #include "polynomial.h"
 
 class Sylvester{
-	int msize, fsize;
+	int msize, fsize; // msize το μέγεθος του πίνακα και fsize το μέγεθος της κάθε συνάρτησης
 	int*** array;
 	public:
 		Sylvester(Polynomial f1, Polynomial f2);
